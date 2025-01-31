73d79e607bb60ea416942d04feccc225684ce388
RokPre <prezelj.rok@gmail.com>
2025-01-31 14:05:30 +0100

    Lua line better time with sentece

--- lua/plugins/luaLine.lua
+++ lua/plugins/luaLine.lua
@@ -22,14 +22,20 @@ return {
                               'data' },
                 lualine_y = { },
                 lualine_z = { function()
-                                local time = os.date("*t") -- Get the current time
-                                local quarter_symbols = { "¼", "½", "¾", "∅∅"} -- Quarter symbols
-                                local quarter_index = math.floor((time.min + 7.5) / 15) -- Determine the quarter index
-                                if quarter_index == 4 then
-                                  time.hour = (time.hour + 1) % 24 -- Handle hour overflow
-                                  quarter_index = 0
-                                end
-                                return string.format(" %02d:%s", time.hour, quarter_symbols[quarter_index + 1])
+                                local time = os.date("*t")
+                                local hour = time.hour % 12
+                                local minute = time.min
+                                local hours = { "Midnight", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Noon" }
+
+                                local phrases = {
+                                  [0] = string.format("%s o'clock", hours[hour+1]),
+                                  [1] = string.format("Quarter past %s", hours[hour+ 1]),
+                                  [2] = string.format("Half past %s", hours[hour + 1]),
+                                  [3] = string.format("Quarter to %s", hours[hour + 2])
+                                }
+                                local minute_index = math.floor((minute + 7.5) % 60 / 15) -- Round to nearest quarter
+
+                                return phrases[minute_index] -- quarter past, half past, quarter to
                               end,
                               'data' }
 
