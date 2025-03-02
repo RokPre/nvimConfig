f89bda0e503b3e6370aac6bba22bf16ae77ebd17
Rok Prezelj <prezelj.rok@gmail.com>
2025-03-02 20:04:43 +0100

    Improved bufferline nvim config. Keymaps no longer in the keymaps file, but in the bufferline.lua plugin file.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,5 +22,8 @@ require("lazy").setup({
   spec = {
 	  import = "config.pluginlist"
   },
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    notify = false
+  },
 })
--- lua/plugins/bufferLine.lua
+++ lua/plugins/bufferLine.lua
@@ -1,13 +1,43 @@
 return {
-  'akinsho/bufferline.nvim',
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    require('bufferline').setup({
-      options = {
-        -- https://github.com/rewhile/CatNvim/blob/33f8c15bb44a61ce35617675c7bdc44fed625621/lua/plugins/bufferline.lua
-        -- Add config here
-      }
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  keys = {
+    -- { "", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+    -- { "", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+    -- { "", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+    -- { "", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    { "<A-e>", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+    { "<A-q>", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+    { "<C-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+    { "<C-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+    { "<C-e>", "<cmd>BufferLinePick<cr>", desc = "Pick buffer" },
+    { "<C-w>", "<cmd>BufferLinePickClose<cr>", desc = "Pick buffer to close" },
+  },
+  opts = {
+    options = {
+      always_show_bufferline = false,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+        {
+          filetype = "snacks_layout_box",
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+    -- Fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
     })
-  end
+  end,
 }
-
